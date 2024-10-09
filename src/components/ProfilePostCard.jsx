a46e1bf2d8126f8141d4725d75813594eaf2ba72
mcvitiesdeez <ott_94@hotmail.com>
2024-10-09 11:04:25 +0800

    Implemented Firebase NoSQL

--- src/components/ProfilePostCard.jsx
+++ src/components/ProfilePostCard.jsx
@@ -1,50 +1,30 @@
 import { Button, Col, Row, Image } from "react-bootstrap"
-import { useState, useEffect } from 'react';
-import axios from "axios";
-import { jwtDecode } from "jwt-decode";
+import { useState, useContext } from 'react';
+import { useDispatch } from 'react-redux';
+import { likePost, removeLikeFromPost } from '../features/posts/postsSlice';
+import { AuthContext } from "./AuthProvider";
 
-export default function ProfilePostCard({ content, postId }) {
-    const [likes, setLikes] = useState([])
+export default function ProfilePostCard({ post }) {
+    const { content, id: postId } = post;
+    const [likes, setLikes] = useState(post.likes || [])
+    const dispatch = useDispatch();
+    const { currentUser } = useContext(AuthContext);
+    const userId = currentUser.uid;
 
-    const token = localStorage.getItem("authToken");
-    const decode = jwtDecode(token);
-    const userId = decode.id;
+    const isLiked = likes.includes(userId);
 
     const pic = "https://pbs.twimg.com/profile_images/1587405892437221376/h167Jlb2_400x400.jpg";
-    const BASE_URL = "https://8ab5a041-0ba3-4f7b-8a44-36be1bedff84-00-3ttw70bhq1idl.sisko.replit.dev";
-
-    useEffect(() => {
-        fetch(`${BASE_URL}/likes/post/${postId}`)
-            .then((response) => response.json())
-            .then((data) => setLikes(data))
-            .catch((error) => console.error("Error:", error));
-    }, [postId]);
-
-    const isLiked = likes.some((like) => like.user_id === userId);
 
     const handleLike = () => (isLiked ? removeFromLikes() : addToLikes());
 
     const addToLikes = () => {
-        axios.post(`${BASE_URL}/likes`, {
-            user_id: userId,
-            post_id: postId
-        })
-            .then((response) => {
-                setLikes([...likes, { ...response.data, likes_id: response.data.id }]);
-            })
-            .catch((error) => console.error("Error: ", error))
+        setLikes([...likes, userId]);
+        dispatch(likePost({ userId, postId }));
     }
 
     const removeFromLikes = () => {
-        const like = likes.find((like) => like.user_id === userId);
-
-        if (like) {
-            axios.put(`${BASE_URL}/likes/${userId}/${postId}`)
-                .then(() => {
-                    setLikes(likes.filter((likeItem) => likeItem.user_id !== userId));
-                })
-                .catch((error) => console.error("Error: ", error))
-        }
+        setLikes(likes.filter((id) => id !== userId));
+        dispatch(removeLikeFromPost({ userId, postId }));
     }
 
     return (
