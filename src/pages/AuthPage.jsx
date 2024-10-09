1c23027a0c086c67459096abf9f1717c5d9c1446
mcvitiesdeez <ott_94@hotmail.com>
2024-10-09 21:15:23 +0800

    Added facebook signup

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -73,6 +73,9 @@ export default function AuthPage() {
                     <Button className='rounded-pill' variant='outline-dark'>
                         <i className='bi bi-apple'></i> Sign up with Apple
                     </Button>
+                    <Button className='rounded-pill' variant='outline-dark'>
+                        <i className='bi bi-facebook'></i> Sign up with Facebook
+                    </Button>
                     <p style={{ textAlign: "center" }}>or</p>
                     <Button className='rounded-pill' onClick={handleShowSignUp}>Create an account</Button>
                     <p style={{ fontSize: "12px" }}>By signing up, you agree to the Terms of Service and Privacy Policy including Cookie Use.</p>
